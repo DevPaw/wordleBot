a1051869c5f500cdcb5668d0a500b9ee04364ad4
DevPaw <adityarpawar212@gmail.com>
2026-06-19 22:06:27 +0530

    made the word list better, fixed common edgecases

--- botV1.py
+++ botV1.py
@@ -9,7 +9,7 @@ with open ('wordle-allowed-guesses.txt', 'r') as f:
 
 plausibleWords = supersetWords.copy()
 
-while not guessed: 
+while guessed == False:
 
     currWord = input("What word did u enter?: ")
 
@@ -18,7 +18,7 @@ while not guessed:
     currYellowPlaces = list(map(int, input("Yellows: ").split()))
     currGreenPlaces = list(map(int, input("Green: ").split()))
 
-    if len(currGreenPlaces) == 4:
+    if len(currGreenPlaces) == 5:
         guessed == True
     else: 
         for i in currGrayPlaces:
@@ -30,28 +30,35 @@ while not guessed:
         for i in currGreenPlaces:
             greenLetters.append(currWord[i])
 
-        if grayLetters:
+        if currGrayPlaces:
             for grayLetter in grayLetters:
                 for word in plausibleWords.copy():
                     if grayLetter in word:
                         plausibleWords.remove(word)
 
-        if yellowLetters:
+        if currYellowPlaces:
             for yellowLetter in yellowLetters:
                 for word in plausibleWords.copy():
                     if yellowLetter not in word:
                         plausibleWords.remove(word)
                     if yellowLetter in word:
-                        if currWord.index(yellowLetter) == word.index(yellowLetter):
+                        if word[currWord.index(yellowLetter)] == yellowLetter:
                             plausibleWords.remove(word)
+                    
     
-        if greenLetters:
+        if currGreenPlaces:
             for greenLetter in greenLetters:
                 for word in plausibleWords.copy():
                     if greenLetter in word: 
-                        if currWord.index(greenLetter) != word.index(greenLetter):
+                        if word[currWord.index(greenLetter)] != greenLetter:
                             plausibleWords.remove(word)
                     else:
                         plausibleWords.remove(word)
 
-        print(plausibleWords)
+        currGrayPlaces = []
+        currYellowPlaces = []
+        currGreenPlaces = []
+
+        yellowLetters = []
+
+        print(plausibleWords, "\n You have ", len(plausibleWords), " more options left")
